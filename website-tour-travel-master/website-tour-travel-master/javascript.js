11c0f337b394f4c51f9dba96b5173400a20fd0d0
Hari Rizky Ardiantoro <155425837+Haruku24@users.noreply.github.com>
2024-07-15 19:20:43 +0700

    javascript.js

--- website-tour-travel-master/website-tour-travel-master/javascript.js
+++ website-tour-travel-master/website-tour-travel-master/javascript.js
@@ -1,25 +1,62 @@
+//menu
+var tombolMenu = $(".tombol-menu");
+var menu = $("nav .menu ul");
+
+function klikMenu() {
+    tombolMenu.click(function () {
+        menu.toggle();
+    });
+    menu.click(function () {
+        menu.toggle();
+    });
+}
+
 $(document).ready(function () {
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 50) {
-        $("nav").addClass("putih");
-        $(".logo img").attr("src", "asset/logo-black.png");
-      } else {
-        $("nav").removeClass("putih");
-        $(".logo img").attr("src", "asset/logo-white.png");
-      }
-    });
-  
-    $(".tombol-menu").click(function () {
-      $(".menu ul").toggleClass("show");
-    });
-  });
-  
-  let currentEventIndex = 0;
-  
-  function moveEvent(step) {
-    const events = document.querySelectorAll('.event-list .event');
-    const totalEvents = events.length;
-    currentEventIndex = (currentEventIndex + step + totalEvents) % totalEvents;
-    document.querySelector('.event-list').style.transform = `translateX(-${currentEventIndex * 100}%)`;
-  }
-  
+    var width = $(window).width();
+    if (width < 990) {
+        klikMenu();
+    }
+})
+
+//check lebar
+$(window).resize(function () {
+    var width = $(window).width();
+    if (width > 989) {
+        menu.css("display", "block");
+        //display:block
+    } else {
+        menu.css("display", "none");
+    }
+    klikMenu();
+});
+
+//efek scroll 
+$(document).ready(function () {
+    var scroll_pos = 0;
+    $(document).scroll(function () {
+        scroll_pos = $(this).scrollTop();
+        if (scroll_pos > 0) {
+            $("nav").addClass("putih");
+            $("nav img.hitam").show();
+            $("nav img.putih").hide();
+        } else {
+            $("nav").removeClass("putih");
+            $("nav img.hitam").hide();
+            $("nav img.putih").show();
+        }
+    })
+});
+
+// Add event listeners for navigation buttons
+document.querySelector('.nav-button.left').addEventListener('click', function() {
+    // Logic for sliding left
+});
+
+document.querySelector('.nav-button.right').addEventListener('click', function() {
+    // Logic for sliding right
+});
+
+// Open transaksi.html in a new tab
+document.getElementById('pesanTiket').addEventListener('click', function() {
+    window.open('transaksi.html', '_blank');
+});
